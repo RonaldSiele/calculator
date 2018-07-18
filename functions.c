535e80e7fef1dfd3c61ede6b9df0e40a5112862d
RonaldSiele <41368937+RonaldSiele@users.noreply.github.com>
2018-07-18 16:34:04 +0300

    Add files via upload

--- functions.c
+++ functions.c
@@ -4,3 +4,15 @@
 int add(int a, int b){
 	return a+b;
 }
+int sub(int a, int b){
+	return a-b;
+}
+int multi(int a, int b){
+	return a*b;
+}
+int quot(int a, int b){
+	return a/b;
+}
+int modu(int a, int b){
+	return a%b;
+}
--- functions.h
+++ functions.h
@@ -1,3 +1,18 @@
-//function declaration
-int add(int a, int b);
+//#include "functions.h"
 
+//a function implementation
+int add(int a, int b){
+	return a+b;
+}
+int sub(int a, int b){
+	return a-b;
+}
+int multi(int a, int b){
+	return a*b;
+}
+int quot(int a, int b){
+	return a/b;
+}
+int modu(int a, int b){
+	return a%b;
+}
